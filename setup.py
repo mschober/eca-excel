367ff5a101b534300ca61bf4fd598b6a7cd5508b
Michael Schober <mschober@expedia.com>
2016-01-18 13:16:27 -0800

    adding setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='eca-excel-openpyxl',
+      version='1.0.0',
+      description='Working with Excel',
+      author='Michael Schober',
+      author_email='mschober@expedia.com',
+      packages=[]
+     )
